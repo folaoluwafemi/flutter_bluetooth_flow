3ec42f354fc3eb668a5d3b7c80af800d0fc9b573
folaoluwfemi <folaoluwafemi55@gmail.com>
2024-02-24 20:36:34 +0100

    fix issues required to work

--- example/android/app/src/main/kotlin/com/dartgod/flutter_bluetooth_flow_example/data/bluetooth_viewmodel.kt
+++
@@ -1,6 +0,0 @@
-package com.dartgod.flutter_bluetooth_flow_example.data
-
-class BluetoothViewModel {
-
-
-}
--- lib/flutter_bluetooth_flow.dart
+++ lib/flutter_bluetooth_flow.dart
@@ -1,6 +1,7 @@
 library flutter_bluetooth_flow;
 
+export 'bluetooth_interface.dart';
 export 'client/bluetooth_client_interface.dart' show BluetoothClient;
+export 'domain/model/bluetooth_device.dart';
+export 'domain/model/scan_mode.dart';
 export 'server/bluetooth_server_interface.dart' show BluetoothServer;
-export  'domain/model/bluetooth_device.dart';
-export  'domain/model/scan_mode.dart';
